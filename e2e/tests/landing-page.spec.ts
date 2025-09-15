1264fa7df15056fceaacf318011a28a79dfa3d86
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-09-15 15:05:52 +0200

    feat: add Playwright tests for Rezumer website

---
+++ e2e/tests/landing-page.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://rezumer.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Rezumer/);
+});
+
+test("sign in link", async ({ page }) => {
+  await page.goto("https://rezumer.com/");
+
+  // Click the sign in link.
+  await page.getByRole("link", { name: "Sign in" }).click();
+
+  // Expects page to have a heading with the name of Continue to rezumer.
+  await expect(page.getByRole("heading", { name: "Continue to rezumer" })).toBeVisible();
+});
